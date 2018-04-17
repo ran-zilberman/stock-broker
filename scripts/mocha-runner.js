2eaf3f7c869c9c6bcee932906113cda018e7db3e
ranzwix <ranz@wix.com>
2018-04-17 23:23:27 +0300

    mocha runner + mocha-setup.ts

---
+++ scripts/mocha-runner.js
@@ -0,0 +1,19 @@
+'use strict';
+
+require('ts-node/register');
+const path = require('path');
+const Mocha = require('mocha');
+const glob = require('glob')
+require(path.join(process.cwd(), 'test', `mocha-setup.ts`));
+
+const options = {
+    timeout: 30000
+};
+
+let mocha = new Mocha({...options})
+
+glob.sync('src/**/*spec.ts').forEach((file) => {
+  mocha.addFile(file)
+})
+
+mocha.run()
